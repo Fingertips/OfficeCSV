e9cabe7998b0977f34f2aae1ad83d8c1603beec1
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2014-03-19 10:45:34 +0100

    Stop using FasterCSV and update encoding

--- lib/office_csv.rb
+++ lib/office_csv.rb
@@ -1,17 +1,19 @@
-require 'fastercsv'
+require 'csv'
 require 'iconv'
 
 class OfficeCSV
-  # Works just like FasterCSV.generate. It adds a byte order mark to the beginning of the document and
+  BOM = [0xff, 0xfe].pack('C*').force_encoding('UTF-16LE')
+
+  # Works just like CSV.generate. It adds a byte order mark to the beginning of the document and
   # converts the contents from UTF-8 to UTF-16LE.
   def self.generate(&block)
-    [0xff, 0xfe].pack('C*') + Iconv.iconv('UTF-16LE', 'UTF-8',
-      FasterCSV.generate(:col_sep => "\t", &block)
+    BOM + Iconv.iconv('UTF-16LE', 'UTF-8',
+      CSV.generate(:col_sep => "\t", &block)
     ).join
   end
   
-  # Just like FastCSV.parse. It removes the byte order mark and transcodes from UTF-16LE to UTF-8.
+  # Just like CSV.parse. It removes the byte order mark and transcodes from UTF-16LE to UTF-8.
   def self.parse(csv)
-    FasterCSV.parse(Iconv.iconv('UTF-8', 'UTF-16LE', csv).first[3..-1], :col_sep => "\t")
+    CSV.parse(Iconv.iconv('UTF-8', 'UTF-16LE', csv).first[1..-1], :col_sep => "\t")
   end
 end
